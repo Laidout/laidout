a86b3fce16bb841512fcfb7e15e70645c886b5d4
tomlechner <tomlechner@users.noreply.github.com>
2007-06-10 06:04:50 +0000

    Adding abstract base classes for file in and out filters.

---
+++ src/filetypes/filefilters.h
@@ -0,0 +1,65 @@
+//
+// $Id$
+//	
+// Laidout, for laying out
+// Please consult http://www.laidout.org about where to send any
+// correspondence about this software.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// For more details, consult the COPYING file in the top directory.
+//
+// Copyright (C) 2004-2007 by Tom Lechner
+//
+#ifndef FILEFILTERS_H
+#define FILEFILTERS_H
+
+#include <lax/anxapp.h>
+
+
+//------------------------------------- FileFilter -----------------------------------
+typedef void Plugin; //******must implement plugins!!
+class FileFilter : public Laxkit::anObject
+{
+ public:
+	Plugin *plugin; //***which plugin, if any, the filter came from.
+	FileFilter() { plugin=NULL; }
+	virtual ~FileFilter() {}
+	virtual const char *Author() = 0;
+	virtual const char *FilterVersion() = 0;
+	
+	virtual const char *Format() = 0;
+	virtual const char **FormatVersions(int *n) = 0;
+	virtual const char *VersionName(const char *version) = 0;
+	virtual const char *FilterClass() = 0;
+
+	virtual Laxkit::anXWindow *ConfigDialog() { return NULL; }
+};
+
+
+//------------------------------------- FileInputFilter -----------------------------------
+class FileInputFilter : public FileFilter
+{
+ public:
+	virtual const char *whattype() { return "FileInputFilter"; }
+	virtual const char *FileType(const char *first100bytes) = 0;
+	virtual int In(const char *file, Laxkit::anObject *context) = 0;
+};
+
+
+//------------------------------------- FileOutputFilter -----------------------------------
+class FileOutputFilter : public FileFilter
+{
+ public:
+	virtual const char *whattype() { return "FileOutputFilter"; }
+	virtual int Out(const char *file, Laxkit::anObject *context) = 0;
+	virtual int Verify(Laxkit::anObject *context) = 0; //preflight checker
+};
+
+
+
+#endif
+
+
