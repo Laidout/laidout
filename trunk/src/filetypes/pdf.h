0a01ef6a1b9f5f7cca96ac980c3ea2e778002c2a
tomlechner <tomlechner@4294d3fa-7b33-4226-8839-2a655ff33960>
2007-07-03 04:15:46 +0000

    Beginning work on pdf export filter.

---
+++ trunk/src/filetypes/pdf.h
@@ -0,0 +1,50 @@
+//
+// $Id$
+//	
+// Laidout, for laying out
+// Please consult http://www.laidout.org about where to send any
+// correspondence about this software.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// For more details, consult the COPYING file in the top directory.
+//
+// Copyright (C) 2007 by Tom Lechner
+//
+#ifndef FILETYPES_PDF_H
+#define FILETYPES_PDF_H
+
+
+
+void installPdfFilter();
+
+
+//------------------------------------ PdfOutputFilter ----------------------------------
+class PdfOutputFilter : public ExportFilter
+{
+ protected:
+	int pdf_version;
+	virtual int pdfdumpobj(FILE *f,double *mm,SomeData *obj,char **error_ret,int &warning);
+ public:
+	PdfOutputFilter(int which);
+	virtual ~PdfOutputFilter() {}
+	virtual const char *Author() { return "Laidout"; }
+	virtual const char *FilterVersion() { return LAIDOUT_VERSION; }
+	
+	virtual const char *DefaultExtension() { return "pdf"; }
+	virtual const char *Format() { return "Pdf"; }
+	virtual const char *Version();
+	virtual const char *VersionName();
+	virtual const char *FilterClass() { return "document"; }
+
+	virtual int Out(const char *filename, Laxkit::anObject *context, char **error_ret);
+
+	//virtual Laxkit::anXWindow *ConfigDialog() { return NULL; }
+	//virtual int Verify(Laxkit::anObject *context); //preflight checker
+};
+
+
+#endif
+
