36c08579c08f1baedff4bae0ea6979167053ed26
tomlechner <tomlechner@4294d3fa-7b33-4226-8839-2a655ff33960>
2010-06-27 20:08:54 +0000

    Adding workaround for patch rendering settings not being automatically used by all instances.

--- trunk/src/dataobjects/limagepatch.cc
+++ trunk/src/dataobjects/limagepatch.cc
@@ -23,6 +23,7 @@ using namespace std;
 
 using namespace LaxInterfaces;
 
+//------------------------------------- LImagePatchInterface -------------------------
 /*! \class LImagePatchInterface
  * 
  * Subclass LaxInterfaces::ImagePatchInterface so that a change to recurse affects
@@ -46,17 +47,56 @@ anInterface *LImagePatchInterface::duplicate(anInterface *dup)
 int LImagePatchInterface::CharInput(unsigned int ch,const char *buffer,int len,unsigned int state,const Laxkit::LaxKeyboard *k)
 {
 	DBG cerr <<"*****************in LImagePatchInterface::CharInput"<<endl;
-	int r=recurse;
+	int r=recurse,m=rendermode;
 	int cc=ImagePatchInterface::CharInput(ch,buffer,len,state,k);
-	if (cc==1) return 1;
-	if (recurse!=r) {
+	if (recurse!=r || m!=rendermode) {
 		for (int c=0; c<laidout->interfacepool.n; c++) {
 			if (!strcmp(laidout->interfacepool.e[c]->whattype(),"ImagePatchInterface")) {
 				static_cast<ImagePatchInterface *>(laidout->interfacepool.e[c])->recurse=recurse;
+				static_cast<ImagePatchInterface *>(laidout->interfacepool.e[c])->rendermode=rendermode;
 				break;
 			}
 		}
 	}
+	if (cc==1) return 1;
 	return cc;
 }
 
+//------------------------------------- LColorPatchInterface -------------------------
+/*! \class LColorPatchInterface
+ * 
+ * Subclass LaxInterfaces::ColorPatchInterface so that a change to recurse affects
+ * the pool class as well. 
+ *
+ * \todo *** this is a big hack... need a better way to control interface data drawing,
+ *   if there is a draw in place, then a draw on top, then should be a flag to not draw
+ *   the one beneath...
+ */
+
+LColorPatchInterface::LColorPatchInterface(int nid,Laxkit::Displayer *ndp)
+	: ColorPatchInterface(nid,ndp)
+{
+}
+
+anInterface *LColorPatchInterface::duplicate(anInterface *dup)
+{
+	return ColorPatchInterface::duplicate(new LColorPatchInterface(id,NULL));
+}
+
+int LColorPatchInterface::CharInput(unsigned int ch,const char *buffer,int len,unsigned int state,const Laxkit::LaxKeyboard *k)
+{
+	DBG cerr <<"*****************in LColorPatchInterface::CharInput"<<endl;
+	int r=recurse,m=rendermode;
+	int cc=ColorPatchInterface::CharInput(ch,buffer,len,state,k);
+	if (recurse!=r || m!=rendermode) {
+		for (int c=0; c<laidout->interfacepool.n; c++) {
+			if (!strcmp(laidout->interfacepool.e[c]->whattype(),"ColorPatchInterface")) {
+				static_cast<ColorPatchInterface *>(laidout->interfacepool.e[c])->recurse=recurse;
+				static_cast<ColorPatchInterface *>(laidout->interfacepool.e[c])->rendermode=rendermode;
+				break;
+			}
+		}
+	}
+	if (cc==1) return 1;
+	return cc;
+}
