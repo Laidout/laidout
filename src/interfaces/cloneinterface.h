a584c4f94abd470d1929e18f6329073c4e504ecd
tomlechner <tomlechner@users.noreply.github.com>
2012-12-17 04:16:03 +0000

    Beginning work on various new interfaces

---
+++ src/interfaces/cloneinterface.h
@@ -0,0 +1,22 @@
+//
+// $Id$
+//	
+// Laidout, for laying out
+// Please consult http://www.laidout.org about where to send any
+// correspondence about this software.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// For more details, consult the COPYING file in the top directory.
+//
+// Copyright (C) 2011 by Tom Lechner
+//
+
+
+
+namespace Laidout {
+
+} //namespace Laidout
+
