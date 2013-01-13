a8b2457177bd3bf3c54409542ed6d57cd0cfcd5c
tomlechner <tomlechner@users.noreply.github.com>
2013-01-13 05:03:38 +0000

    Making rudimentary base class for c++ based plugins

--- src/plugins/plugin.cc
+++ src/plugins/plugin.cc
@@ -11,12 +11,11 @@
 // version 2 of the License, or (at your option) any later version.
 // For more details, consult the COPYING file in the top directory.
 //
-// Copyright (C) 2012 by Tom Lechner
+// Copyright (C) 2013 by Tom Lechner
 //
 
 
-********this file is not active yet!!!*****************
-
+#include "plugin.h"
 
 
 namespace Laidout {
@@ -28,13 +27,12 @@ namespace Laidout {
  * main window panes
  * commands (possible return value, optional parameter dialog)
  * random dialogs (command with optional context)
- * image import filters
+ * image import filters (todo)
  * import filters
  * export filters
  * interfaces and possibly new data types
  * impositions
  * resources, like page sizes, palettes, imposition instance templates, etc
- * icons/menu items
  * particular templates/types of data, master pages for instance
  * interpreters (python, yacas, octave, tex/latex?  -> investigate swig)
  * 
@@ -42,37 +40,14 @@ namespace Laidout {
  */
 
 
-class PluginBase : public Laxkit::anObject
+PluginBase::PluginBase()
 {
-  public:
-	virtual const char *PluginName()  = 0;
-	virtual const char *Version()     = 0;
-	virtual const char *Description() = 0;
-	virtual const char *Author()      = 0;
-	virtual const char *ReleaseDate() = 0;
-	virtual const char *License()     = 0;
+}
 
-	virtual unsigned long WhatYouGot();
+PluginBase::~PluginBase()
+{
+}
 
-	 //return NULL terminated list
-	virtual LaidoutDialog     **WindowPanes() { return NULL; }
-	virtual ImageImportFilter **ImageImportFilters() { return NULL; }
-	virtual ImportFilter      **ImportFilters() { return NULL; }
-	virtual ExportFilter      **ExportFilters() { return NULL; }
-	virtual anInterface       **Tools() { return NULL; } //must specify what window types they should appear in
-	virtual LaidoutAction     **Actions() { return NULL; }//maybe scripted code actions to be available for key bindings in windows
-	virtual Interpreter       **Interpreters() { return NULL; } //like python. selecting "Run" of a PlainTextObject uses these
-
-	virtual Resource          **ResourceInstances() { return NULL; }
-	virtual Imposition        **Impositions() { return NULL; }
-	virtual Config            **Configs() { return NULL; } //tool settings and global Laidout config
-	virtual DrawableObject    **ObjectInstances() { return NULL; } //like scrapbook items
-
-	virtual CalculatorModule  **CalculatorModules() { return NULL; }
-	
-	PluginBase();
-	virtual ~PluginBase();
-};
 
 
 } // namespace Laidout
