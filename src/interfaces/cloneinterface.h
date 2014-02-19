7e2b4bf7d437c64f65448166c363b5b257abe0f5
tomlechner <tomlechner@users.noreply.github.com>
2014-02-19 05:25:36 +0000

    Implemented Tiling load/save.

--- src/interfaces/cloneinterface.h
+++ src/interfaces/cloneinterface.h
@@ -162,7 +162,9 @@ class Tiling : public Laxkit::anObject, public LaxFiles::DumpUtility //, public 
 //					   LaxInterfaces::ViewportWindow *viewport
 //					   );
 	
+	virtual ObjectDef *makeObjectDef();
 	virtual void dump_out(FILE *f,int indent,int what,Laxkit::anObject *context);
+	virtual LaxFiles::Attribute *dump_out_atts(LaxFiles::Attribute *att,int what,Laxkit::anObject *context);
 	virtual void dump_in_atts(LaxFiles::Attribute *att, int, Laxkit::anObject*);
 };
 
