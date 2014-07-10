d52dc46506ff494bb1e0c1201b3340143c0ba92d
tomlechner <tomlechner@4294d3fa-7b33-4226-8839-2a655ff33960>
2014-07-10 14:01:36 +0000

    continued tinkering to make plugin operational. (not there yet)

--- trunk/src/plugins/plugin.h
+++ trunk/src/plugins/plugin.h
@@ -29,6 +29,25 @@ namespace Laidout {
 class PluginBase : public Laxkit::anObject
 {
   public:
+	enum PluginBaseContents {
+		PLUG_Panes           = (1<<0),
+		PLUG_ImageImports    = (1<<1),
+		PLUG_ImportFilters   = (1<<2),
+		PLUG_ExportFilters   = (1<<3),
+		PLUG_Tools           = (1<<4),
+		PLUG_Actions         = (1<<5),
+		PLUG_Interpreters    = (1<<6),
+		PLUG_Resources       = (1<<7),
+		PLUG_Impositions     = (1<<8),
+		PLUG_Configs         = (1<<9),
+		PLUG_DrawableObjects = (1<<10),
+		PLUG_Calculators     = (1<<11),
+		PLUG_MAX
+	};
+
+	virtual const char *Id_str() { return object_idstr; }
+	virtual unsigned long Id() { return object_id; }
+
 	virtual const char *PluginName()  = 0;
 	virtual const char *Version()     = 0;
 	virtual const char *Description() = 0;
@@ -37,23 +56,23 @@ class PluginBase : public Laxkit::anObject
 	virtual const char *License()     = 0;
 	virtual LaxFiles::Attribute *OtherMeta() = 0;
 
-	virtual unsigned long WhatYouGot() = 0;
+	virtual unsigned long WhatYouGot() = 0; //or'd list of PluginBaseContents
 
 	 //return NULL terminated list
 	virtual Laxkit::NewWindowObject    **WindowPanes() { return NULL; }
-	//virtual ImageImportFilter        **ImageImportFilters() { return NULL; }
 	virtual ImportFilter               **ImportFilters() { return NULL; }
 	virtual ExportFilter               **ExportFilters() { return NULL; }
 	virtual LaxInterfaces::anInterface **Tools() { return NULL; } //must specify what window types they should appear in
-	//virtual LaidoutAction            **Actions() { return NULL; }//maybe scripted code actions to be available for key bindings in windows
-	//virtual Interpreter                **Interpreters() { return NULL; } //like python. selecting "Run" of a PlainTextObject uses these
-
-	//virtual Resource                   **ResourceInstances() { return NULL; }
 	virtual Imposition                 **Impositions() { return NULL; }
-	//virtual Config                   **Configs() { return NULL; } //tool settings and global Laidout config
 	virtual DrawableObject             **ObjectInstances() { return NULL; } //like scrapbook items
+	virtual CalculatorModule           **CalculatorModules() { return NULL; }
+	virtual Interpreter                **Interpreters() { return NULL; } //like python. selecting "Run" of a PlainTextObject uses these
 
-	virtual CalculatorModule  **CalculatorModules() { return NULL; }
+	//virtual ImageImportFilter        **ImageImportFilters() { return NULL; }
+	//virtual LaidoutAction            **Actions() { return NULL; }//maybe scripted code actions to be available for key bindings in windows
+	//virtual Resource                   **ResourceInstances() { return NULL; }
+	//virtual Config                   **Configs() { return NULL; } //tool settings and global Laidout config
+
 	
 	PluginBase();
 	virtual ~PluginBase();
