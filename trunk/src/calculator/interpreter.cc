2df96047b7758f231c6ac16b2bee39ef0fada99d
tomlechner <tomlechner@4294d3fa-7b33-4226-8839-2a655ff33960>
2014-07-20 23:33:47 +0000

    Further interpreter brainstorming

--- trunk/src/calculator/interpreter.cc
+++ trunk/src/calculator/interpreter.cc
@@ -65,15 +65,48 @@ class LaidoutCalculator : public Interpreter
 {  .... };
 
 
+
+
+//---------------------------------- Interpreter ---------------------------------------
+/*! \class Interpreter
+ * \brief Base class for all extra interpreter language bindings.
+ */
+
+Interpreter::Interpreter()
+{
+	source_plugin=NULL;
+}
+
+Interpreter::~Interpreter()
+{
+	if (source_plugin) source_plugin->dec_count();
+}
+
+
+void Interpreter::dump_out(FILE *f,int indent,int what,Laxkit::anObject *savecontext)
+{
+	Attribute att;
+	dump_out_atts(&att, what, savecontext);
+	att.dump_out(f,indent);
+}
+
+
+
 //------------------------------------PythonInterpreter--------------------------------------
 class PythonInterpreter : public Interpreter
 {
- public:
-	
+	int gil_state;
+	int gil_init;
+
+  public:
+
+	PythonInterpreter();
+	virtual ~PythonInterpreter();
+
 	const char *Id() { return "python"; }
 	const char *Name() { return "Python"; }
-	const char *Description() { return _("The python interpreter. See python.org for more."); }
-	const char *Version() { return "1.0"; }
+	const char *Description() { return _("A Python interpreter. See python.org for more."); }
+	const char *Version() { return "0.1"; }
 
 	virtual int InitInterpreter();
 	virtual int CloseInterpreter();
@@ -84,14 +117,39 @@ class PythonInterpreter : public Interpreter
 	void ClearError() = 0;
 };
 
+PythonInterpreter::PythonInterpreter()
+{
+	gil_init=0;
+	gil_state=-1;
+}
+
+PythonInterpreter::~PythonInterpreter()
+{
+}
+
 //! Return 0 for interpreter ready to go, or nonzero for invalid, and cannot run.
 int PythonInterpreter::InitInterpreter(const char *programname)
 {
 	//Py_SetProgramName(programname); //optional
 	Py_Initialize();
 
-	***setup the Laidout module
 
+
+
+	**** beware threads!!! put this somewhere relevant...
+	if (!gil_init) {
+		gil_init = 1;
+		PyEval_InitThreads();
+		PyEval_SaveThread();
+	}
+	gil_state = PyGILState_Ensure();
+	// Call Python/C API functions...    
+	PyGILState_Release(gil_state);
+
+			
+
+	 //setup the Laidout module
+	*** create LaidoutMethods
 	Py_InitModule("Laidout",LaidoutMethods);
 
 
@@ -145,41 +203,7 @@ void PythonInterpreter::ClearError()
 
 
 
-//---------------------------------- Interpreter ---------------------------------------
-/*! \class Interpreter
- * \brief Base class for all extra interpreter language bindings.
- */
-class Interpreter : public Laxkit::anObject, public LaxFiles::DumpUtility
-{
- public:
-	Plugin *source_plugin; //if the interpreter came from a plugin
 
-	const char *Id() = 0;
-	const char *Name() = 0;
-	const char *Description() = 0;
-	const char *Version() = 0;
-	
-	Interpreter();
-	virtual ~Interpreter();
-	virtual int InitInterpreter() = 0;
-	virtual int CloseInterpreter() = 0;
-
-	void KillScript(); //run script in separate thread, so, In would be called, then
-					   //laidout would continue on its way, until a message comes saying
-					   //that the script is all done. In the meantime, this function could
-					   //be called during execution to kill bad scripts
-					   //***probably that level of care should be entirely within the
-					   //laidout code that uses interpreters, not the interpreter itself
-
-	 //return status: 0 success, -1 success with warnings, 1 fatal error
-	int In(const char *input_text, char **result_ret, char **error_ret) = 0;
-	char *GetLastResult() = 0;
-	char *GetError(int *input_index_ret) = 0;
-	void ClearError() = 0;
-
-	dump_out() {}     //for optionally saving any initialization state or history 
-	dump_in_atts() {}
-};
 
 
 
--- trunk/src/calculator/interpreter.h
+++ trunk/src/calculator/interpreter.h
@@ -15,9 +15,49 @@
 //
 
 
+#include <lax/anobject.h>
+#include <lax/dump.h>
 
 namespace Laidout {
 
+
+//---------------------------------- Interpreter ---------------------------------------
+/*! \class Interpreter
+ * \brief Base class for all extra interpreter language bindings.
+ */
+class Interpreter : public Laxkit::anObject, public LaxFiles::DumpUtility
+{
+ public:
+	Plugin *source_plugin; //if the interpreter came from a plugin
+
+	const char *Id() = 0;
+	const char *Name() = 0;
+	const char *Description() = 0;
+	const char *Version() = 0;
+	
+	Interpreter();
+	virtual ~Interpreter();
+	virtual int InitInterpreter() = 0;
+	virtual int CloseInterpreter() = 0;
+
+	void KillScript(); //run script in separate thread, so, In would be called, then
+					   //laidout would continue on its way, until a message comes saying
+					   //that the script is all done. In the meantime, this function could
+					   //be called during execution to kill bad scripts
+					   //***probably that level of care should be entirely within the
+					   //laidout code that uses interpreters, not the interpreter itself
+
+	 //return status: 0 success, -1 success with warnings, 1 fatal error
+	int In(const char *input_text, char **result_ret, char **error_ret) = 0;
+	char *GetLastResult() = 0;
+	char *GetError(int *input_index_ret) = 0;
+	void ClearError() = 0;
+
+    virtual void       dump_out(FILE *f,int indent,int what,Laxkit::anObject *savecontext);
+    virtual Attribute *dump_out_atts(Attribute *att,int what,Laxkit::anObject *savecontext) = 0;
+    virtual void dump_in_atts(Attribute *att,int flag,Laxkit::anObject *loadcontext) =0;
+
+};
 
 
 
