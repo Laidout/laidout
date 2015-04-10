cf8b3ea59499ba3b4872a60796d9e480a9e024bb
tomlechner <tomlechner@4294d3fa-7b33-4226-8839-2a655ff33960>
2015-04-10 03:16:37 +0000

    Made about be accessible via the shortcut window.

--- trunk/src/helpwindow.cc
+++ trunk/src/helpwindow.cc
@@ -16,9 +16,11 @@
 
 #include <lax/messagebar.h>
 #include <lax/button.h>
+#include <lax/tabframe.h>
 
 #include <lax/shortcutwindow.h>
 
+#include "about.h"
 #include "language.h"
 #include "headwindow.h"
 #include "helpwindow.h"
@@ -37,20 +39,41 @@ namespace Laidout {
 
 //------------------------ newHelpWindow -------------------------
 
+class HelpAbout : public Laxkit::TabFrame
+{
+  public:
+	HelpAbout()
+		: TabFrame(NULL,"Help",_("Help"),
+				   ANXWIN_REMEMBER|ANXWIN_ESCAPABLE| BOXSEL_LEFT|BOXSEL_TOP|BOXSEL_ONE_ONLY|BOXSEL_ROWS,
+				   0,0,600,600,0, NULL,0,NULL)
+		{}
+	virtual ~HelpAbout() {}
+	virtual const char *whattype() { return "Help"; }
+};
 
 Laxkit::anXWindow *newHelpWindow(const char *place)
 {
+	TabFrame *frame=new HelpAbout();
+
 	laidout->InitializeShortcuts();
 	ShortcutManager *manager=GetDefaultShortcutManager();
 	if (isblank(manager->setfile)) {
 		makestr(manager->setfile,laidout->config_dir);
 		appendstr(manager->setfile,"/default.keys");
 	}
-	ShortcutWindow *win=new ShortcutWindow(NULL,"Shortcuts","Shortcuts",
-					ANXWIN_REMEMBER|ANXWIN_ESCAPABLE|SHORTCUTW_Show_Search|SHORTCUTW_Load_Save,
+	ShortcutWindow *shortcutwin=new ShortcutWindow(frame,"Shortcuts",_("Shortcuts"),
+					ANXWIN_REMEMBER|SHORTCUTW_Show_Search|SHORTCUTW_Load_Save,
 					0,0,400,600,0,place);
-	makestr(win->textheader,"#\n# Laidout shortcuts\n#\n");
-	return win;
+	makestr(shortcutwin->textheader,"#\n# Laidout shortcuts\n#\n");
+
+	frame->AddWin(shortcutwin, 1, _("Shortcuts"), NULL, 0);
+
+
+	AboutWindow *about=new AboutWindow(frame);
+	frame->AddWin(about, 1, _("About"), NULL, 0);
+	
+
+	return frame;
 }
 
 //------------------------ HelpWindow -------------------------
