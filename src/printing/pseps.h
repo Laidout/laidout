3a69c8e34e0e3e7a9534fbeda3fe27ae6b2fb563
Tom Lechner <tom@tomlechner.com>
2018-03-03 14:22:59 -0800

    Switch to gpl3

--- src/printing/pseps.h
+++ src/printing/pseps.h
@@ -1,12 +1,11 @@
 //
-// $Id$
 //	
 // Laidout, for laying out
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
+// version 3 of the License, or (at your option) any later version.
 // For more details, consult the COPYING file in the top directory.
 //
 // Please consult http://www.laidout.org about where to send any
--- src/stylemanager.cc
+++ src/stylemanager.cc
@@ -1,5 +1,4 @@
 //
-// $Id$
 //	
 // Laidout, for laying out
 // Copyright (C) 2012 by Tom Lechner
@@ -7,7 +6,7 @@
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public
 // License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
+// version 3 of the License, or (at your option) any later version.
 // For more details, consult the COPYING file in the top directory.
 //
 // Please consult http://www.laidout.org about where to send any
