0f97dd57e613a77ff8d0167286b85b05bb6ebb22
Tom Lechner <tom@tomlechner.com>
2019-02-02 22:34:23 -0800

    Moved main node files to their own directory

---
+++ src/nodes/nodes-dataobjects.h
@@ -0,0 +1,36 @@
+//
+// Laidout, for laying out
+// Please consult http://www.laidout.org about where to send any
+// correspondence about this software.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+// For more details, consult the COPYING file in the top directory.
+//
+// Copyright (C) 2019 by Tom Lechner
+//
+#ifndef NODES_DATAOBJECTS_H
+#define NODES_DATAOBJECTS_H
+
+
+#include <lax/objectfactory.h>
+#include "nodeinterface.h"
+
+
+
+namespace Laidout { 
+
+
+//--------------------- Setup --------------------------------
+int SetupDataObjectNodes(Laxkit::ObjectFactory *factory);
+
+
+
+} //namespace Laidout
+
+
+#endif
+
+
