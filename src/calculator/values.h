f71369ad0f9ac06b0485f4cb977a50d5980cc4f2
Tom Lechner <tom@tomlechner.com>
2021-01-05 16:45:20 -0800

    SetValue::Remove. Also moved ValueConstraint to laxkit.

--- src/calculator/values.h
+++ src/calculator/values.h
@@ -139,53 +139,6 @@ class SimpleFunctionEvaluator : public FunctionEvaluator
 						 Laxkit::ErrorLog *log);
 };
 
-//------------------------ ValueConstraint --------------------------------
-class ValueConstraint
-{
-  public:
-	enum Constraint {
-		PARAM_None = 0,
-
-		PARAM_No_Maximum,
-		PARAM_No_Minimum,
-		PARAM_Min_Loose_Clamp, //using the <, <=, >, >= should be hints, not hard clamp
-		PARAM_Max_Loose_Clamp, //using the <, <=, >, >= should be hints, not hard clamp
-		PARAM_Min_Clamp, //when numbers exceed bounds, force clamp
-		PARAM_Max_Clamp, //when numbers exceed bounds, force clamp
-
-		PARAM_Integer,
-
-		//PARAM_Step_Adaptive_Mult,
-		PARAM_Step_Adaptive_Add,
-		PARAM_Step_Add,  //sliding does new = old + step, or new = old - step
-		PARAM_Step_Mult, //sliding does new = old * step, or new = old / step
-
-		PARAM_MAX
-	};
-
-	int value_type;
-	Constraint mintype, maxtype, steptype;
-	double min, max, step;
-	double default_value;
-
-	ValueConstraint() {
-		value_type = PARAM_None;
-		default_value = min = max = 0;
-		step=1;
-		mintype = PARAM_No_Maximum;
-		maxtype = PARAM_No_Minimum;
-		steptype = PARAM_Step_Mult;
-	}
-	virtual ~ValueConstraint();
-
-	virtual bool IsValid(Value *v, bool correct_if_possible, Value **v_ret);
-	virtual int SetBounds(const char *bounds); //a single range like "( .. 0]", "[0 .. 1]", "[.1 .. .9]", "{1..9]"
-	virtual int SetBounds(double nmin, int nmin_type, double nmax, int nmax_type);
-	virtual int SetStep(double nstep, Constraint nsteptype);
-
-	virtual int SlideInt(int oldvalue, double numsteps);
-	virtual double SlideDouble(double oldvalue, double numsteps);
-};
 
 //------------------------------ ObjectDef --------------------------------------------
 
@@ -469,6 +422,7 @@ class SetValue : public Value, virtual public FunctionEvaluator
 	virtual int type() { return VALUE_Set; }
 
 	virtual int Push(Value *v,int absorb, int where=-1);
+	virtual int Remove(int index);
 	virtual int n();
 	virtual Value *e(int i);
 	virtual int Set(int i, Value *v, int absorb);
@@ -708,6 +662,9 @@ class FileValue : public Value, virtual public FunctionEvaluator
 	char seperator;
 	Laxkit::PtrStack<char> parts;
 	char *filename;
+	bool hint_is_dir;
+	bool hint_for_saving;
+	
 	FileValue(const char *f=NULL,int len=-1);
 	virtual ~FileValue();
 	virtual const char *whattype() { return "FileValue"; }
